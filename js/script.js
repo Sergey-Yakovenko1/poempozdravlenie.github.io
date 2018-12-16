bc6a1baf80033cf0f304ab9afbe1d3343cd16835
Sergey-Yakovenko1 <serhii.yakovenko1@gmail.com>
2018-12-16 21:26:18 +0200

    first commit
    add to file

---
+++ js/script.js
@@ -0,0 +1,483 @@
+var copyEmailBtn = document.querySelector('.copybtn1');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem1');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn2');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem2');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn3');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem3');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn4');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem4');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn5');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem5');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn6');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem6');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn7');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem7');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn8');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem8');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn9');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem9');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn10');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem10');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn11');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem11');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn12');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem12');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn13');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem13');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn14');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem14');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn15');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem15');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn16');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem16');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn17');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem17');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn18');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem18');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn19');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem19');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn20');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem20');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
+var copyEmailBtn = document.querySelector('.copybtn21');  
+copyEmailBtn.addEventListener('click', function(event) {  
+  // Выборка ссылки с электронной почтой 
+  var emailLink = document.querySelector('.copypoem21');  
+  var range = document.createRange();  
+  range.selectNode(emailLink);  
+  window.getSelection().addRange(range);  
+    
+  try {  
+    // Теперь, когда мы выбрали текст ссылки, выполним команду копирования
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+  } catch(err) {  
+    console.log('Oops, unable to copy');  
+  }  
+    
+  // Снятие выделения - ВНИМАНИЕ: вы должны использовать
+  // removeRange(range) когда это возможно
+  window.getSelection().removeAllRanges();  
+});
+//конец скрипта
+
